2ce55cf84b1530ee55248d126153d6b4ddf114be
mariangelamarasciuolo <130611642+mariangelamarasciuolo@users.noreply.github.com>
2023-11-03 16:20:03 +0100

    Initial commit

---
+++ src/main/java/progettos5l5/mariangelamarasciuolo/Progettos5l5Application.java
@@ -0,0 +1,13 @@
+package progettos5l5.mariangelamarasciuolo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Progettos5l5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Progettos5l5Application.class, args);
+	}
+
+}
---
+++ src/test/java/progettos5l5/mariangelamarasciuolo/Progettos5l5ApplicationTests.java
@@ -0,0 +1,13 @@
+package progettos5l5.mariangelamarasciuolo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Progettos5l5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
